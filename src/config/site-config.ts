a95c1a0997047714c61494a39a45ccc9d8df6eab
Sebastian Olate H <seba.olate.h@gmail.com>
2025-07-20 20:47:06 -0400

    feat: integracion metatags con componente MetaData

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,4 +1,13 @@
 export const siteConfig = {
   title: "SebaOlate",
   description: 'Sebastian Olate Huenuñir, Front-end developer. Portafolio profesional',
+  keywords: [
+    'Sebastian Olate',
+    'SebaOlate',
+    'Front-end developer',
+    'Web developer',
+    'Desarrollador web',
+    'Front-end',
+    'Portafolio',
+  ],
 };
